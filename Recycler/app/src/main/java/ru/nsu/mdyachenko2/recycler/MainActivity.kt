f12d5c7afba21f8dd1c2e9ba42de143a2b71933d
m.dyachenko2 <m.dyachenko2@g.nsu.ru>
2024-11-06 00:18:26 +0700

    begin

---
+++ Recycler/app/src/main/java/ru/nsu/mdyachenko2/recycler/MainActivity.kt
@@ -0,0 +1,16 @@
+package ru.nsu.mdyachenko2.recycler
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.nsu.mdyachenko2.recycler.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
